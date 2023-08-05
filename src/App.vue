1ebd2b864d72d8989668ee59decdaf2e088e1685
AveryNewhart <newhartreeceavery@gmail.com>
2023-08-05 18:44:46 -0400

    basic layout complete

--- src/App.vue
+++ src/App.vue
@@ -4,8 +4,27 @@
 <template>
   <div>
     <h1>final countdown</h1>
+    <h2>(display time here)</h2>
+      <div>
+        <label for="hours">Hour(s):</label>
+        <input type="number" id="hours" min="0" max="24" />
+        <label for="minutes">Minute(s):</label>
+        <input type="number" id="minutes" min="0" max="60" />
+        <label for="seconds">Second(s):</label>
+        <input type="number" id="seconds" min="0" max="60" />
+      </div>
+      <div class="but-div">
+        <button>Start</button>
+        <button>Pause</button>
+        <button>Restart</button>
+      </div>
   </div>
 </template>
 
 <style scoped>
+
+.but-div {
+  margin-top: 15px;
+}
+
 </style>
