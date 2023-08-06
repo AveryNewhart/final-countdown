c9fde242002916af2335aed783d6c3fb7986eae5
AveryNewhart <newhartreeceavery@gmail.com>
2023-08-06 16:17:25 -0400

    starting of basic functions

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,36 @@
 <script setup lang="ts">
+import { ref } from 'vue';
+
+const startingTime = ref("hours" | "minutes" | "seconds");
+
+// setting the state of which the time is
+const state = ref<"stopped" | "running" | "paused">("stopped");
+
+function Start() {
+
+}
+
+function Pause() {
+
+}
+
+function Reset() {
+
+}
+
+function formatTime(startingTime: number) {
+  const hours = `0${Math.floor(startingTime / 24)}`.slice(-2);
+  const minutes = `0${Math.floor(startingTime / 60)}`.slice(-2);
+  const seconds = `0${startingTime % 60}`.slice(-2);
+  return `${hours}:${minutes}:${seconds}`;
+}
+
 </script>
 
 <template>
   <div>
     <h1>final countdown</h1>
-    <h2>(display time here)</h2>
+    <h2>(display time here){{ formatTime(startingTime) }}</h2>
       <div>
         <label for="hours">Hour(s):</label>
         <input type="number" id="hours" min="0" max="24" />
@@ -16,7 +42,7 @@
       <div class="but-div">
         <button>Start</button>
         <button>Pause</button>
-        <button>Restart</button>
+        <button>Reset</button>
       </div>
   </div>
 </template>
